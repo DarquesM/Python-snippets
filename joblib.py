c2b3f7760e6f4a79fc7ee27e5593f50c077e4eca
DarquesM <docteur.zuf@gmail.com>
2017-05-15 21:58:37 +0200

    Create joblib.py

---
+++ joblib.py
@@ -0,0 +1,13 @@
+#Snippet for parallel jobs
+
+from joblib import Parallel, delayed
+import multiprocessing
+
+def foo(p)
+    #do stuff
+    return something
+
+from joblib.pool import has_shareable_memory
+num_cores = multiprocessing.cpu_count()
+
+Parallel(n_jobs=num_cores,max_nbytes=1e6)(delayed(has_shareable_memory)(foo(p)) for p in parameters)
