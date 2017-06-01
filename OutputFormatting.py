233a1cb5e1e5d4f6cb4d7fd303bf069a12d9a4fc
DarquesM <docteur.zuf@gmail.com>
2017-06-01 22:32:59 +0200

    Update OutputFormatting.py

--- OutputFormatting.py
+++ OutputFormatting.py
@@ -1,7 +1,11 @@
 #Examples str.format() method usage
 
 print ("Ceci {0} {1} {2} test %s".format("est", "un", "petit")%("de print"))
+# output : Ceci est un petit test de print
 
 print ("%(x)s %(x)s %(x)s" % {"x" : "bon"})
+# output : bon bon bon
 
 print("3a={} 3b={} 3c={} {}".format(*"chut"))
+# output : 3a=c 3b=h 3c=u t
+
